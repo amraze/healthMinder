a178e9785114a3a3de72ed141434454e24a9d00d
Ahmed Mestiri <ahmed.mestiri@supcom.tn>
2023-01-30 14:23:23 +0100

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllergiesComponent } from './dashboard/allergies/allergies.component';
+import { DoctorsComponent } from './dashboard/doctors/doctors.component';
+import { ForumComponent } from './dashboard/forum/forum.component';
+import { HomepageComponent } from './dashboard/homepage/homepage.component';
+import { RemindersComponent } from './dashboard/reminders/reminders.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Dashboard Routes
+  { path: 'allergies', component: AllergiesComponent },
+  { path: 'doctors', component: DoctorsComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'reminders', component: RemindersComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // Global Routes
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotfoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DoctorsComponent } from './dashboard/doctors/doctors.component';
+import { RemindersComponent } from './dashboard/reminders/reminders.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { ForumComponent } from './dashboard/forum/forum.component';
+import { AllergiesComponent } from './dashboard/allergies/allergies.component';
+import { HomepageComponent } from './dashboard/homepage/homepage.component';
+import { DefaultLayoutComponent } from './dashboard/default-layout/default-layout.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DoctorsComponent,
+    RemindersComponent,
+    SettingsComponent,
+    ForumComponent,
+    AllergiesComponent,
+    HomepageComponent,
+    DefaultLayoutComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
